a132535f2395834b6261288837e8280fcacaf5a9
Boris Kaminskiy <bk-ops-1@bk.ru>
2024-09-25 10:08:10 +0300

    feat: lazy pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,18 +1,87 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route, Navigate } from 'react-router'
-import { Layout, Main, About, Partners, Services, Payment, Delivery, ShoppingCart } from '~/pages'
+import { Layout } from '~/pages'
+import { Spiner } from '~/ui'
 
+const MainLazy = lazy(() => import('../pages/Main/Main'))
+
+const AboutLazy = lazy(() => import('../pages/About/About'))
+const PartnersLazy = lazy(() => import('../pages/Partners/Partners'))
+const ServicesLazy = lazy(() => import('../pages/Services/Services'))
+const PaymentLazy = lazy(() => import('../pages/Payment/Payment'))
+const DeliveryLazy = lazy(() => import('../pages/Delivery/Delivery'))
+const ShoppingCartLazy = lazy(
+  () => import('../pages/ShoppingCart/ShoppingCart'),
+)
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="" element={<Layout />}>
-        <Route path="/" index element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/partners" element={<Partners />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/delivery" element={<Delivery />} />
-        <Route path="/shoppingcart" element={<ShoppingCart />} />
+        <Route
+          path="/"
+          index
+          element={
+            <Suspense fallback={<Spiner />}>
+              <MainLazy />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/about"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <AboutLazy />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/partners"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <PartnersLazy />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/services"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <ServicesLazy />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/payment"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <PaymentLazy />
+            </Suspense>
+          }
+        />
+
+        <Route
+          path="/delivery"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <DeliveryLazy />
+            </Suspense>
+          }
+        />
+        
+        <Route
+          path="/shoppingcart"
+          element={
+            <Suspense fallback={<Spiner />}>
+              <ShoppingCartLazy />{' '}
+            </Suspense>
+          }
+        />
+
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
