585441191e66a52ee866faaed8f484aa02f04901
Boris Kaminskiy <bk-ops-1@bk.ru>
2024-09-25 09:44:46 +0300

    feat: lazy pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,26 +1,27 @@
+import { lazy } from 'react'
 import { Routes, Route, Navigate } from 'react-router'
-import {
-  Layout,
-  Main,
-  About,
-  Partners,
-  Services,
-  Payment,
-  Delivery,
-  ShoppingCart,
-} from '~/pages'
+import { Layout, Main } from '~/pages'
+
+const AboutLazy = lazy(() => import('../pages/About/About'))
+const PartnersLazy = lazy(() => import('../pages/Partners/Partners'))
+const ServicesLazy = lazy(() => import('../pages/Services/Services'))
+const PaymentLazy = lazy(() => import('../pages/Payment/Payment'))
+const DeliveryLazy = lazy(() => import('../pages/Delivery/Delivery'))
+const ShoppingCartLazy = lazy(
+  () => import('../pages/ShoppingCart/ShoppingCart'),
+)
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" index element={<Main />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/partners" element={<Partners />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/delivery" element={<Delivery />} />
-        <Route path="/shoppingcart" element={<ShoppingCart />} />
+        <Route path="/about" element={<AboutLazy />} />
+        <Route path="/partners" element={<PartnersLazy />} />
+        <Route path="/services" element={<ServicesLazy />} />
+        <Route path="/payment" element={<PaymentLazy />} />
+        <Route path="/delivery" element={<DeliveryLazy />} />
+        <Route path="/shoppingcart" element={<ShoppingCartLazy />} />
         <Route path="*" element={<Navigate to={'/'} />} />
       </Route>
     </Routes>
